8e92eac0120f3b31c857893d486cc9afa1cfd7ab
Ринат Аюпов <rinat.ayup@mail.ru>
2019-04-16 23:48:25 +0300

    Отредактирован Program.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,9 +7,16 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Accounts 2.0\n***** Добро пожаловать в программу учёта записей! *****\n");
-            
+            // Используйте класс Manipulator, мне было лень делать интерфейс!
+
+            Console.WriteLine("Registries 2.0\n***** Добро пожаловать в программу для создания реестров! *****\n");
+
+            Manipulator.SaveRegistriesList("Базовый список");
             Manipulator.LoadRegistriesList("Базовый список");
+
+            // Дальше можете создавать реестры используя методы класса Manipulator!!!
+            // Не забудьте сохранить потом измененния используя "Manipulator.SaveRegistriesList"!!!
+            
 
             Console.ReadLine();
         }
